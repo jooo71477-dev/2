9bcbf27a1894b05c5d69c8bada6eac53f84d7f0a
jooo71477-dev <jooo71477@gmail.com>
2026-04-04 13:06:58 +0200

    Update

--- sw.js
+++ sw.js
@@ -8,6 +8,7 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
